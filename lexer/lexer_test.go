227c23adafd9ab37834517f98a443c794eff600d
Meqdad Amr <Meqdad_@outlook.com>
2023-08-07 18:58:33 +0300

    1.4 Adding support for if, else, == and !=

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -6,7 +6,7 @@ import (
 	"Monkey/token"
 )
 
-func TestNextToken(t *testing.T) {
+func TestLexer_NextToken1(t *testing.T) {
 	input := `=+(){},;`
 
 	tests := []struct {
@@ -41,7 +41,7 @@ func TestNextToken(t *testing.T) {
 
 }
 
-func TestNextToken2(t *testing.T) {
+func TestLexer_NextToken2(t *testing.T) {
 	input := `let five = 5;
  				let ten = 10;
 				let add = fn(x, y) {
@@ -108,7 +108,7 @@ func TestNextToken2(t *testing.T) {
 	}
 }
 
-func TestLexer_NextToken(t *testing.T) {
+func TestLexer_NextToken3(t *testing.T) {
 	input := `let five = 5;
 let ten = 10;
 
@@ -190,4 +190,83 @@ let result = add(five, ten);
 		}
 	}
 
+}
+
+func TestLexer_NextToken4(t *testing.T) {
+	input := `if (5 < 10) {
+	return true;
+} else {
+	return false;
+}`
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+	}
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
+
+func TestLexer_NextToken5(t *testing.T) {
+	input := `	10 == 10;
+				10 != 9;	`
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.INT, "10"},
+		{token.EQ, "=="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "10"},
+		{token.NOT_EQ, "!="},
+		{token.INT, "9"},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
+	}
 }
