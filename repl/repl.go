db2ccde773d784d3f9b2f603df91749ea7db0a1a
Meqdad Amr <Meqdad_@outlook.com>
2023-08-14 19:17:52 +0300

    3.5 Evaluating Expressions

--- repl/repl.go
+++ repl/repl.go
@@ -1,6 +1,7 @@
 package repl
 
 import (
+	"Monkey/evaluator"
 	"Monkey/lexer"
 	"Monkey/parser"
 	"bufio"
@@ -47,7 +48,10 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
