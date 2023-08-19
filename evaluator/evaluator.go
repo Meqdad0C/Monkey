2d7857f43ae6833fabc17bc0b636e30c856361dc
Meqdad Amr <Meqdad_@outlook.com>
2023-08-19 23:28:14 +0300

    4.4 Evaluate Array

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -63,6 +63,13 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		body := node.Body
 		return &object.Function{Parameters: params, Env: env, Body: body}
 
+	case *ast.ArrayLiteral:
+		elements := evalExpressions(node.Elements, env)
+		if len(elements) == 1 && isError(elements[0]) {
+			return elements[0]
+		}
+		return &object.Array{Elements: elements}
+
 	// Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
